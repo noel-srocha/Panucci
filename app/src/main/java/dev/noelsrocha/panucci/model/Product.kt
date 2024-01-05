5da01c5f0d53bb18da47bfa38362a18f90128426
Manoel de Souza Rocha Neto <manoelrocha.n93@gmail.com>
2024-01-05 20:04:18 -0300

    Initial commit

---
+++ app/src/main/java/dev/noelsrocha/panucci/model/Product.kt
@@ -0,0 +1,10 @@
+package dev.noelsrocha.panucci.model
+
+import java.math.BigDecimal
+
+class Product(
+    val name: String,
+    val price: BigDecimal,
+    val description: String,
+    val image: String? = null
+)
